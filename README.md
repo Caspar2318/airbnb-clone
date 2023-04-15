a993b16487faaa46d3f61d896ace8de0ed33ffca
Caspar2318 <108163945+Caspar2318@users.noreply.github.com>
2023-04-15 10:38:28 +1000

    Listing reservation

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Live address at:
 
 ---
 
-This one will practice using newest version of app router, so we choose no for 'src' folder, but use 'app' folder instead. The problem will meet along the way is that react will confuse to use client component or server component. Inside app folder, most of them will be defaulted set as server component, so we need to define current component is client component by type "use client" at the top of each component code.
+This one will practice using newest version of app router, so we choose no for 'src' folder, but use 'app' folder instead. The problem will meet along the way is that react will confuse to use client component or server component. Inside app folder, most of them will be defaulted set as server component, so we need to define current component is client component by type "use client" at the top of each component code. Especially for those .tsx files which has used hooks.
 
 ---
 
@@ -12,7 +12,9 @@ Issue will be met as:
 
 Since next 13 is new and 'app' folder is under experimental phase, so we'll met a problem as 'Unhandled Runtime Error' as a popup error, but the app is still able to run. This error comes from 'react-hydration-error'
 
-Solutions: we can use 'useEffect' inside single tsx file to remove the error. In side this project, a specific file as 'ClientOnly.tsx' is created inside app/component folder, and then go to app/layout.tsx file to wrap components by <ClientOnly> component.
+Solutions: we can use 'useEffect' inside single tsx file to remove the error. Inside this project, a specific file as 'ClientOnly.tsx' is created inside app/component folder, and then go to app/layout.tsx file to wrap components by <ClientOnly> component.
+
+---
 
 => to create app:
 
@@ -28,7 +30,7 @@ Other packages need to install:
 
 => npm install -D prisma => npx prisma init
 
-=> npm install next-auth @prima/client @next-auth/prisma-adapter bcrypt => npm install -D @types/bcrypt => npm install leaflet => npm install -D @types/leaflet
+=> npm install next-auth @prima/client @next-auth/prisma-adapter bcrypt => npm install -D @types/bcrypt => npm install leaflet => npm install -D @types/leaflet => npm install react-date-range => npm install -D @types/react-date-range
 
 ---
 
@@ -45,3 +47,5 @@ Code in order as:
 => Categories UI with categories of properties setting
 
 => Categories functionalities: 1. category selection 2. location and map implement 3. Counter page for rooms and guests 4. upload images (using cloudinary, add cloudinary env variables inside .env, variable must start with formart as "NEXT*PUBLIC_CLOUDINARY*...", random named variables will not works) 5. description and price listing 6. submit the form to creat list
+
+=> Listing: 1. create Listing UI to upload personal property to advertised it to Airbnb 2. fetch listing data from server side 3. favorite listing function 4. listing detail page 5. listing reservation function
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,15 +1,29 @@
 "use client";
-import { useMemo } from "react";
+import { differenceInCalendarDays, eachDayOfInterval } from "date-fns";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Range } from "react-date-range";
+
 import { SafeUser, SafeListing } from "@/app/types";
 import { Reservation } from "@prisma/client";
-import React from "react";
+
 import categories from "@/app/components/navbar/constant";
 import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
 import ListingInfo from "@/app/components/listings/ListingInfo";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import ListingReservation from "@/app/components/listings/ListingReservation";
+
+const initialDateRange = {
+  startDate: new Date(),
+  endDate: new Date(),
+  key: "selection",
+};
 
 interface ListingClientProps {
-  reservation?: Reservation[];
+  reservations?: Reservation[];
   listing: SafeListing & {
     user: SafeUser;
   };
@@ -17,10 +31,74 @@ interface ListingClientProps {
 }
 
 const ListingClient = ({
-  reservation,
+  reservations = [],
   listing,
   currentUser,
 }: ListingClientProps) => {
+  const loginModal = useLoginModal();
+  const router = useRouter();
+
+  const disabledDates = useMemo(() => {
+    let dates: Date[] = [];
+
+    reservations.forEach((reservation) => {
+      const range = eachDayOfInterval({
+        start: new Date(reservation.startDate),
+        end: new Date(reservation.endDate),
+      });
+
+      dates = [...dates, ...range];
+    });
+
+    return dates;
+  }, [reservations]);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(listing.price);
+  const [dateRange, setDateRange] = useState<Range>(initialDateRange);
+
+  // to create reservation
+  const onCreateReservation = useCallback(() => {
+    if (!currentUser) return loginModal.onOpen();
+
+    setIsLoading(true);
+    axios
+      .post("/api/reservations", {
+        totalPrice,
+        startDate: dateRange.startDate,
+        endDate: dateRange.endDate,
+        listingId: listing?.id,
+      })
+      .then(() => {
+        toast.success(
+          "Successful! Property Reserved, enjoy your holiday with Airbnb. "
+        );
+        setDateRange(initialDateRange);
+        // redirect to /trips
+        router.refresh();
+      })
+      .catch(() => {
+        toast.error("Something went wrong...");
+      })
+      .finally(() => setIsLoading(false));
+  }, [currentUser, loginModal, totalPrice, dateRange, listing?.id, router]);
+
+  // update total price while changing date selection
+  useEffect(() => {
+    if (dateRange.startDate && dateRange.endDate) {
+      const dayCount = differenceInCalendarDays(
+        dateRange.endDate,
+        dateRange.startDate
+      );
+
+      if (dayCount && listing.price) {
+        setTotalPrice(dayCount * listing.price);
+      } else {
+        setTotalPrice(listing.price);
+      }
+    }
+  }, [dateRange, listing.price]);
+
   const category = useMemo(() => {
     return categories.find((item) => item.label === listing.category);
   }, [listing.category]);
@@ -46,6 +124,17 @@ const ListingClient = ({
               bathroomCount={listing.bathroomCount}
               locationValue={listing.locationValue}
             />
+            <div className="order-first mb-10 md:order-last md:col-span-3">
+              <ListingReservation
+                price={listing.price}
+                totalPrice={totalPrice}
+                onChangeDate={(value) => setDateRange(value)}
+                dateRange={dateRange}
+                onSubmit={onCreateReservation}
+                disabled={isLoading}
+                disabledDates={disabledDates}
+              />
+            </div>
           </div>
         </div>
       </div>
